26298e9c2ace3bd8c849c67a986d765804ea3bc8
Adwaith <adwaithtry@gmail.com>
2024-01-02 20:22:44 +0530

    Furst step

---
+++ RRIMS_Attendance_Manager/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for RRIMS_Attendance_Manager project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "RRIMS_Attendance_Manager.settings")
+
+application = get_asgi_application()
